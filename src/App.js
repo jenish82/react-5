776b2d5e1307e5342dc77a883692b408b246a650
JenishbekJanybaev <dj.jenish82@gmail.com>
2025-04-17 19:03:41 +0600

    lesson-5

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [events, setEvents] = useState([
+    {title: "akhror's birthday party", id: 1},
+    {title: "doniyor's live stream", id: 2},
+    {title: "match: manchester united vs barcelona", id: 3}
+  ])
+
+  let [name, setName] = useState('My name is Jenish')
+  
+  const addClic = () =>{
+    setName('My name is Kenesh')
+    console.log(name)
+  }
+
+  const adres = "https://studio.youtube.com/channel/UCoAVxWLM7WuPv0kjOIgXSww"
+  
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {events.map((event)=>{
+        return(
+          <div key={event.id}>
+            <h2>{event.title}</h2>
+          </div>
+
+        )
+      })
+      }
+
+
+      <h1>{name}</h1>
+      <p>Random namber:{Math.floor(Math.random() * 100)}</p>
+      <a href={adres}>My web site</a>
+      <button onClick={addClic}>add</button>
+      <br />
+      <button onClick={() => {console.log('va aleykum assalam')}}>clic</button>
+      <button onClick={()=> console.log('rahmatullohi va baracatu')}>alic</button>
+
+
     </div>
   );
 }
